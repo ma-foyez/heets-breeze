20c0ceb69ba0ab4db95f655b59d30ce55c5d5efd
AnikRifat <58522653+AnikRifat@users.noreply.github.com>
2024-05-28 23:05:24 +0600

    Update ProductSeeder.php

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -21,27 +21,28 @@ class ProductSeeder extends Seeder
                 'image' => '1.jpg',
                 'product_category' => 1, // Assuming 1 is the ID for Iqoo category
                 'order' => 1,
+                'regular_price' => '120 AED','offer_price' => '100 AED'
             ],
             [
                 'title' => 'Iqoo HeatStick Silver',
                 'description' => 'Enjoy the crisp and clean taste of Iqoo HeatStick Silver. Perfect for a refreshing vaping session.',
                 'image' => '2.jpg',
                 'product_category' => 1,
-                'order' => 2,
+                'order' => 2,'regular_price' => '120 AED','offer_price' => '100 AED'
             ],
             [
                 'title' => 'HEATS Classic Tobacco',
                 'description' => 'Indulge in the classic tobacco flavor with HEATS Classic. A timeless choice for tobacco enthusiasts.',
                 'image' => '3.jpg',
                 'product_category' => 2, // Assuming 2 is the ID for HEATS category
-                'order' => 1,
+                'order' => 1,'regular_price' => '120 AED','offer_price' => '100 AED'
             ],
             [
                 'title' => 'HEATS Menthol Breeze',
                 'description' => 'Feel the cool menthol sensation with HEATS Menthol Breeze. Ideal for a refreshing and invigorating vape.',
                 'image' => '4.jpg',
                 'product_category' => 2,
-                'order' => 2,
+                'order' => 2,'regular_price' => '120 AED','offer_price' => '100 AED'
             ],
         ];
 
